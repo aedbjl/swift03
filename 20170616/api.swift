a58b3a93256c394ac6609db70c008986d3686a62
iii-user <iii-user@iii-userde-MacBook-4.local>
2017-06-16 09:32:52 +0800

    v1

---
+++ 20170616/api.swift
@@ -0,0 +1,23 @@
+//
+//  api.swift
+//  20170616
+//
+//  Created by iii-user on 2017/6/16.
+//  Copyright © 2017年 iii-user. All rights reserved.
+//
+
+import Foundation
+
+class MyClass01{}
+class MyClass02{
+    var x:Int
+    init(x:Int){self.x = x}
+}
+class MyClass03 {
+    var x = 123
+    init(){}
+    
+}
+class MyClass04{
+    var x:Int?
+}
